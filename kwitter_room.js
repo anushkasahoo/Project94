d59f0be522a85773aee9f7544477f76528b4ef67
anushkasahoo <85781997+anushkasahoo@users.noreply.github.com>
2021-07-02 16:52:59 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,11 @@
+
+var firebaseConfig = {
+    apiKey: "AIzaSyCt5nJeS1yz1houkr8owSzOlKCiuZaFd68",
+    authDomain: "project-94-c7083.firebaseapp.com",
+    projectId: "project-94-c7083",
+    storageBucket: "project-94-c7083.appspot.com",
+    messagingSenderId: "751583970122",
+    appId: "1:751583970122:web:a55e307efc609fa6ceb7b3"
+  };
+
+firebase.initializeApp(firebaseConfig);
